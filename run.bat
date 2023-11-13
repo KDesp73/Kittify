e1dc305e8624207450eaa5dd0884e670a5959279
Konstantinos Despoinidis <kdesp2003@gmail.com>
2023-11-13 16:00:51 +0200

    updated run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,23 @@
 @echo off
+setlocal
 
-SET projectpath=%~dp0
-SET jre=%projectpath%\bin\java.exe
+rem Set the path to the bin folder containing java.exe
+set "java_bin_path=bin"
 
-%jre% -jar .\MusicPlayer.jar
+rem Find the JAR file in the current folder
+for %%i in (*.jar) do set "jar_file=%%i"
+
+rem Check if a JAR file was found
+if not defined jar_file (
+    echo No JAR file found in the current folder.
+    exit /b 1
+)
+
+rem Build the command to execute the JAR file
+set "java_command=%java_bin_path%\java.exe -jar "%jar_file%""
+
+rem Execute the command
+%java_command%
+
+endlocal
+
